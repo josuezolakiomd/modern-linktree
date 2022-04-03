27b5f49bdc8e183ec7aa44eeea0cf73d9f4371aa
Josue Zolakio <josuezolakiocanal@gmail.com>
2022-04-03 11:43:07 +0100

    About me added on the header section

---
+++ src/assets/script/script.js
@@ -0,0 +1,11 @@
+// const aboutMe = document.querySelector(".about-me");
+// const profilePhoto = document.querySelector(".profile-photo");
+
+// profilePhoto.addEventListener("mouseover", () => {
+//   if (aboutMe.style.visibility === "visible") {
+//     aboutMe.style.visibility = "hidden";
+//   } else {
+//     aboutMe.style.transition = "ease-in-out 200ms";
+//     aboutMe.style.visibility = "visible";
+//   }
+// });
